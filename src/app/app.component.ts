ad8254c0a27233072ce428473f563519dab11e68
lasbl <lasbleis.olivier@yahoo.fr>
2025-09-29 21:46:58 +0200

    form-chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Chat {
+  nom : string;
+  couleur : COULEUR;
+  age : number;
+}
+
+enum COULEUR {
+  BLANC, GRIS, NOIR
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tp-form';
+  chatList : Chat[] = []
+  newChat : Chat = {
+    nom : "",
+    couleur : COULEUR.BLANC,
+    age : 5
+  }
+
+  ajouterNewChat() : void {
+    this.chatList.push({...this.newChat})
+  }
 }
